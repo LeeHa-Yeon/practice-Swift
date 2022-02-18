3dd06e19c22ad248d0362890edd3b18b3ec8a969
LeeHa-Yeon <gkdus1613@naver.com>
2022-02-18 18:27:39 +0900

    [FEAT] 화면 스크린샷 기능 완료

--- ScreenshotCaptureTest/ScreenshotCaptureTest/ViewController.swift
+++ ScreenshotCaptureTest/ScreenshotCaptureTest/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var captureBtn: UIButton!
+    @IBOutlet var backgroundShadowView: UIView!
+    
+    
+    @IBAction func captureBtnTapped(_ sender: Any) {
+        takeScreenshot(of: backgroundShadowView)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // 스크린 캡쳐하는 함수
+    func takeScreenshot(of view: UIView) {
+        UIGraphicsBeginImageContextWithOptions(
+            CGSize(width: view.bounds.width, height: view.bounds.height),
+            false,
+            2
+        )
 
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let screenshot = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
 
+        UIImageWriteToSavedPhotosAlbum(screenshot, self, #selector(imageWasSaved), nil)
+    }
+    
+    // MARK: - Actions
+    @objc func imageWasSaved(_ image: UIImage, error: Error?, context: UnsafeMutableRawPointer) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+
+        print("Image was saved in the photo gallery")
+        UIApplication.shared.open(URL(string:"photos-redirect://")!)
+    }
+    
 }
 
