a0773774876ffe6171ae55f51573202ebdee34c5
LeeHa-Yeon <gkdus1613@naver.com>
2022-03-22 20:21:56 +0900

    [UPDATE] Custom Alert 수정

--- CustomAlert/CustomAlert/AlertView/AlertView.swift
+++ CustomAlert/CustomAlert/AlertView/AlertView.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class AlertView: UIView {
     
-    
+    var yearArr: [Int] = [2021,2022,2023]
+    var monthArr: [Int] = [1,2,3,4,5,6,7,8,9,10,11,12]
     
     @IBOutlet var customView: UIView!
+    @IBOutlet weak var yearPickerView: UIPickerView!
+    @IBOutlet weak var monthPickerView: UIPickerView!
     @IBOutlet weak var cancleButton: UIButton!
     @IBOutlet weak var okButton: UIButton!
     
-
+    @IBAction func cancleBtnTapped(_ sender: Any) {
+        print("클릭이 되고있니..?")
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
+    }
+    
+    override func layoutSubviews() {
+        setPickerView()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -27,9 +37,12 @@ class AlertView: UIView {
         commonInit()
     }
     
+    
     // autoresizingMask로 모든 화면에 맞춰줌
     private func commonInit() {
+        
         Bundle.main.loadNibNamed("AlertView", owner: self, options: nil)
+        
         cancleButton.layer.borderWidth = 0.5
         cancleButton.layer.borderColor = UIColor.lightGray.cgColor
         
@@ -41,9 +54,92 @@ class AlertView: UIView {
         addSubview(customView)
     }
     
+    func setPickerView(){
+        
+        yearPickerView.delegate = self
+        yearPickerView.dataSource = self
+        monthPickerView.delegate = self
+        monthPickerView.dataSource = self
+        selectedPickerViewUICustom(yearPickerView)
+        selectedPickerViewUICustom(monthPickerView)
+    }
+    
+    func selectedPickerViewUICustom(_ selectedPickerView: UIPickerView) {
+        selectedPickerView.subviews[1].backgroundColor = .clear
+        
+        let upLine = UIView(frame: CGRect(x: -8, y: 0, width: selectedPickerView.layer.frame.width, height: 1.5))
+        let underLine = UIView(frame: CGRect(x: -8, y: 42, width: selectedPickerView.layer.frame.width, height: 1.5))
+        
+        upLine.backgroundColor = .black
+        underLine.backgroundColor = .black
+        
+        selectedPickerView.subviews[1].addSubview(upLine)
+        selectedPickerView.subviews[1].addSubview(underLine)
+    }
+    
+    
     func showAlert(firstBtnTitle: String? = nil, secondBtnTitle: String? = nil){
         cancleButton.setTitle(firstBtnTitle, for: .normal)
         okButton.setTitle(secondBtnTitle, for: .normal)
+    }
+    
+}
+
+extension AlertView: UIPickerViewDataSource, UIPickerViewDelegate {
+    // 피커뷰의 구성요소(컬럼) 수
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // 높이
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 40
+    }
+    
+    // 구성요소(컬럼)의 행수
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == yearPickerView {
+            return yearArr.count
+        } else if pickerView == monthPickerView {
+            return monthArr.count
+        }
+        return 0
+    }
+    
+    //     피커뷰에 보여줄 값 전달
+    //    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    //        print("우웅ㄹ")
+    //        if pickerView == yearPickerView {
+    //            return "\(yearArr[row])년"
+    //        } else if pickerView == monthPickerView {
+    //            return "\(monthArr[row])월"
+    //        }
+    //        return ""
+    //    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        
+        var pickerLabel: UILabel? = (view as? UILabel)
+        if pickerLabel == nil {
+            pickerLabel = UILabel()
+            pickerLabel?.font = UIFont(name: "Goyang", size: 14)
+            pickerLabel?.textAlignment = .center
+        }
+        
+        if pickerView == yearPickerView {
+            pickerLabel?.text = "\(yearArr[row])년"
+        } else if pickerView == monthPickerView {
+            pickerLabel?.text = "\(monthArr[row])월"
+        }
+        
+        return pickerLabel!
+    }
+    
+    
+    //  피커뷰에서 선택시 호출
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("잘 선택되고있니..?ㅠㅠㅠ")
     }
     
 }
--- CustomAlert/CustomAlert/ViewController.swift
+++ CustomAlert/CustomAlert/ViewController.swift
@@ -9,17 +9,67 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var monthArr: [Int] = [1,2,3,4,5,6,7,8,9,10,11,12]
+    
+    @IBOutlet weak var testPickView: UIPickerView!
+    
+    @IBOutlet var mainView: UIView!
     
     @IBAction func testBtn(_ sender: Any) {
         let customView = AlertView()
+        customView.layer.zPosition = 999;
         customView.showAlert(firstBtnTitle: "취소", secondBtnTitle: "확인")
         view.addSubview(customView)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        testPickView.delegate = self
+        testPickView.dataSource = self
     }
 
 
 }
 
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    // 피커뷰의 구성요소(컬럼) 수
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // 높이
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 40
+    }
+    
+    // 구성요소(컬럼)의 행수
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return monthArr.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+
+        var pickerLabel: UILabel? = (view as? UILabel)
+        if pickerLabel == nil {
+            pickerLabel = UILabel()
+            pickerLabel?.font = UIFont(name: "Goyang", size: 14)
+            pickerLabel?.textAlignment = .center
+        }
+        
+        pickerLabel?.text = "\(monthArr[row])월"
+        
+        return pickerLabel!
+    }
+    
+    
+    //     피커뷰에서 선택시 호출
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("dfdf")
+        print(monthArr[row])
+    }
+    
+    
+    
+}
