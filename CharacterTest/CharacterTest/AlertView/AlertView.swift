29b57e155e04ac4fb3ad9e706820b76147936fd7
LeeHa-Yeon <gkdus1613@naver.com>
2022-04-16 01:56:57 +0900

    [FEAT] 커스텀 팝업창 생성, 구현

--- CharacterTest/CharacterTest/AlertView/AlertView.swift
+++ CharacterTest/CharacterTest/AlertView/AlertView.swift
@@ -1,8 +1,106 @@
-//
-//  AlertView.swift
-//  CharacterTest
-//
-//  Created by 이하연 on 2022/04/16.
-//
+import UIKit
 
-import Foundation
+class AlertView: UIView {
+    
+    public var delegate: CustomAlertProtocol?
+    
+    
+    // MARK: - Properties
+    var currentView: UIView!
+    
+    // MARK: - UIComponents
+    
+    @IBOutlet var parentMainView: UIView!
+    
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var okButton: UIButton!
+    
+    
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+        self.delegate?.cancleButtonTapped(currentView)
+    }
+    
+    @IBAction func okButtonTapped(_ sender: UIButton) {
+        self.delegate?.okButtonTapped(currentView)
+    }
+    
+    
+    // MARK: - LifeCycle
+    
+    override func layoutSubviews() {
+        setLayer()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+    
+    // MARK: - Functions
+
+    
+    // 모든 화면에 맞춰주는 작업
+    private func initialize() {
+        Bundle.main.loadNibNamed("AlertView", owner: self, options: nil)
+        parentMainView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        parentMainView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(parentMainView)
+    }
+    
+    // 버튼 테두리 생성
+    func setLayer(){
+        
+        popupView.layer.cornerRadius = 20
+        
+        cancelButton.layer.borderWidth = 1
+        cancelButton.layer.borderColor = UIColor(named: "soulMain")?.cgColor
+        cancelButton.layer.cornerRadius = 27
+        
+        okButton.layer.borderWidth = 1
+        okButton.layer.borderColor = UIColor(named: "soulMain")?.cgColor
+        okButton.layer.cornerRadius = 27
+    }
+    
+    
+    // 팝업 표시
+    func popupAlert(iconStyle: alertIconStyle, title: String ,message: String, firstBtnTitle: String? = "아니요", secondBtnTitle: String? = "예",myView: UIView){
+        
+        switch iconStyle {
+        case .exclamationIcon :
+            iconImageView.image = UIImage(named: "exclamationIcon")
+            break
+        case .questionIcon :
+            iconImageView.image = UIImage(named: "questionIcon")
+            break
+        }
+        
+        titleLabel.text = title
+        messageLabel.text = message
+        
+        cancelButton.setTitle(firstBtnTitle, for: .normal)
+        okButton.setTitle(secondBtnTitle, for: .normal)
+        
+        currentView = myView
+    }
+    
+}
+
+protocol CustomAlertProtocol {
+    func cancleButtonTapped(_ popupView: UIView)
+    func okButtonTapped(_ popupView: UIView)
+}
+
+enum alertIconStyle: String {
+    case exclamationIcon
+    case questionIcon
+}
--- CharacterTest/CharacterTest/HomeViewController.swift
+++ CharacterTest/CharacterTest/HomeViewController.swift
@@ -19,15 +19,30 @@ class HomeViewController: UIViewController {
         $0.target = self
         $0.action = #selector(moveToVC(sender:))
     }
-
+    
+    lazy var testButton = UIButton().then {
+        $0.setTitle("눌러", for: .normal)
+        $0.backgroundColor = .blue
+        $0.addTarget(self, action: #selector(testAlert(_:)), for: .touchUpInside)
+    }
+    
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setLayout()
         setUI()
     }
     
     //MARK: - Functions
-
+    
+    func setLayout(){
+        [testButton].forEach{ self.view.addSubview($0)}
+        
+        testButton.snp.makeConstraints{
+            $0.centerY.centerX.equalToSuperview()
+        }
+    }
+    
     func setUI(){
         self.view.backgroundColor = .white
         setNavigation()
@@ -36,12 +51,32 @@ class HomeViewController: UIViewController {
     func setNavigation(){
         navigationItem.rightBarButtonItem = myRoutinBarButton
     }
-
+    
     //MARK: - objc Functions
     @objc func moveToVC(sender: UIBarButtonItem){
         let CharacterVC = CharacterViewController()
         self.navigationController?.pushViewController(CharacterVC, animated: true)
     }
-
+    
+    @objc func testAlert(_ sender: UIButton){
+        let popupView = AlertView(frame: view.bounds)
+        //        popupView.okButton.isHidden = true
+        popupView.popupAlert(iconStyle: .questionIcon, title: "스티브 잡스를 체험하시겠어요?", message: "셀럽은 하루에 한 명씩만 체험할 수 있습니다.", firstBtnTitle: "뒤로가기", secondBtnTitle: "체험하기", myView: popupView)
+        popupView.delegate = self
+        view.addSubview(popupView)
+    }
 }
 
+extension HomeViewController: CustomAlertProtocol {
+    
+    func cancleButtonTapped(_ popupView: UIView) {
+        popupView.removeFromSuperview()
+    }
+    
+    func okButtonTapped(_ popupView: UIView) {
+        popupView.removeFromSuperview()
+        print("체험중~~")
+    }
+    
+    
+}
