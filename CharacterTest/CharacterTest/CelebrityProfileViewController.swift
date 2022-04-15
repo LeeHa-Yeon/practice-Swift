19388fd791539a2ba2b02a3a8a8123774b2d77c1
LeeHa-Yeon <gkdus1613@naver.com>
2022-04-15 14:12:57 +0900

    [FEAT] Character Profile - 상세 프로필 UI 구현 완료

--- CharacterTest/CharacterTest/CelebrityProfileViewController.swift
+++ CharacterTest/CharacterTest/CelebrityProfileViewController.swift
@@ -11,6 +11,8 @@ import SnapKit
 
 class CelebrityProfileViewController: UIViewController {
     
+    var profileWidth: Int = 60
+    
     //MARK: - UI Components
     lazy var scrollView = UIScrollView().then {
         $0.addSubview(contentView)
@@ -88,7 +90,7 @@ class CelebrityProfileViewController: UIViewController {
         $0.distribution = .equalSpacing
         $0.spacing = 10
         $0.addArrangedSubview(profileLabel)
-        $0.addArrangedSubview(fourView)
+        $0.addArrangedSubview(profileStackView)
     }
     
     var profileLabel = UILabel().then{
@@ -97,15 +99,262 @@ class CelebrityProfileViewController: UIViewController {
         $0.font = UIFont(name: "Pretendard-Bold", size: 14.0)
     }
     
-//    var profileStackView = UIStackView().then {
-//        $0.axis = .vertical
-//        $0.distribution = .equalSpacing
-//        $0.spacing = 12
-//    }
-    
-    var fourView = UIView().then {
-        $0.backgroundColor = .green
+    lazy var profileStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .equalSpacing
+        $0.spacing = 12
+        $0.addArrangedSubview(birthdayStackView)
+        $0.addArrangedSubview(deceasedAtStackView)
+        $0.addArrangedSubview(mbtiStackView)
+        $0.addArrangedSubview(nationalityStackView)
+        $0.addArrangedSubview(bodySpecStackView)
+        $0.addArrangedSubview(educationStackView)
+        $0.addArrangedSubview(jobStackView)
+        $0.addArrangedSubview(wealthStackView)
+        $0.addArrangedSubview(spouseStackView)
+        $0.addArrangedSubview(childrenStackView)
     }
+    
+    lazy var birthdayStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 10
+        $0.alignment = .top
+        $0.addArrangedSubview(birthdayKeyLabel)
+        $0.addArrangedSubview(birthdayValueLabel)
+    }
+    
+    // 60
+    var birthdayKeyLabel = UILabel().then {
+        $0.text = "출생"
+        $0.textColor = UIColor(named: "soulGray1")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    var birthdayValueLabel = UILabel().then {
+        $0.text = "1955.02.24"
+        $0.textColor = UIColor(named: "soulBlack")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    lazy var deceasedAtStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 10
+        $0.alignment = .top
+        $0.addArrangedSubview(deceasedAtKeyLabel)
+        $0.addArrangedSubview(deceasedAtValueLabel)
+    }
+    
+    var deceasedAtKeyLabel = UILabel().then {
+        $0.text = "사망"
+        $0.textColor = UIColor(named: "soulGray1")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    var deceasedAtValueLabel = UILabel().then {
+        $0.text = "2011.10.05"
+        $0.textColor = UIColor(named: "soulBlack")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    lazy var mbtiStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 10
+        $0.alignment = .top
+        $0.addArrangedSubview(mbtiKeyLabel)
+        $0.addArrangedSubview(mbtiValueLabel)
+    }
+    
+    var mbtiKeyLabel = UILabel().then {
+        $0.text = "MBTI"
+        $0.textColor = UIColor(named: "soulGray1")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    var mbtiValueLabel = UILabel().then {
+        $0.text = "ENTJ"
+        $0.textColor = UIColor(named: "soulBlack")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    lazy var nationalityStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 10
+        $0.alignment = .top
+        $0.addArrangedSubview(nationalityKeyLabel)
+        $0.addArrangedSubview(nationalityValueLabel)
+    }
+    
+    var nationalityKeyLabel = UILabel().then {
+        $0.text = "국적"
+        $0.textColor = UIColor(named: "soulGray1")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    var nationalityValueLabel = UILabel().then {
+        $0.text = "미국"
+        $0.textColor = UIColor(named: "soulBlack")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    lazy var bodySpecStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 10
+        $0.alignment = .top
+        $0.addArrangedSubview(bodySpecKeyLabel)
+        $0.addArrangedSubview(bodySpecValueLabel)
+    }
+    
+    var bodySpecKeyLabel = UILabel().then {
+        $0.text = "신체"
+        $0.textColor = UIColor(named: "soulGray1")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    var bodySpecValueLabel = UILabel().then {
+        $0.text = "183cm 59kg"
+        $0.textColor = UIColor(named: "soulBlack")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    lazy var educationStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 10
+        $0.alignment = .top
+        $0.addArrangedSubview(educationKeyLabel)
+        $0.addArrangedSubview(educationValueLabel)
+    }
+    
+    var educationKeyLabel = UILabel().then {
+        $0.text = "학력"
+        $0.textColor = UIColor(named: "soulGray1")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    var educationValueLabel = UILabel().then {
+        $0.text = "리드대학교 철학과 중퇴"
+        $0.textColor = UIColor(named: "soulBlack")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    lazy var jobStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 10
+        $0.alignment = .top
+        $0.addArrangedSubview(jobKeyLabel)
+        $0.addArrangedSubview(jobValueLabel)
+    }
+    
+    var jobKeyLabel = UILabel().then {
+        $0.text = "직업"
+        $0.textColor = UIColor(named: "soulGray1")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    var jobValueLabel = UILabel().then {
+        $0.text = "기업인"
+        $0.textColor = UIColor(named: "soulBlack")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    lazy var wealthStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 10
+        $0.alignment = .top
+        $0.addArrangedSubview(wealthKeyLabel)
+        $0.addArrangedSubview(wealthValueLabel)
+    }
+    
+    var wealthKeyLabel = UILabel().then {
+        $0.text = "재산"
+        $0.textColor = UIColor(named: "soulGray1")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    var wealthValueLabel = UILabel().then {
+        $0.text = "83억 달러(사망 당시)\n219억 달러(2021년 기준)"
+        $0.textColor = UIColor(named: "soulBlack")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    lazy var spouseStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 10
+        $0.alignment = .top
+        $0.addArrangedSubview(spouseKeyLabel)
+        $0.addArrangedSubview(spouseValueLabel)
+    }
+    
+    var spouseKeyLabel = UILabel().then {
+        $0.text = "배우자"
+        $0.textColor = UIColor(named: "soulGray1")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    var spouseValueLabel = UILabel().then {
+        $0.text = "로렌 파월(1991~2011)"
+        $0.textColor = UIColor(named: "soulBlack")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    lazy var childrenStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 10
+        $0.alignment = .top
+        $0.addArrangedSubview(childrenKeyLabel)
+        $0.addArrangedSubview(childrenValueLabel)
+    }
+    
+    var childrenKeyLabel = UILabel().then {
+        $0.text = "자녀"
+        $0.textColor = UIColor(named: "soulGray1")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+    }
+    
+    var childrenValueLabel = UILabel().then {
+        $0.text = "리사 브레넌 잡스\n리드 잡스\n에린 시에나 잡스\n이브 잡스"
+        $0.textColor = UIColor(named: "soulBlack")
+        $0.font = UIFont(name: "Pretendard-Regular", size: 16.0)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
     
     lazy var lastStackView = UIStackView().then {
         $0.axis = .vertical
@@ -132,7 +381,7 @@ class CelebrityProfileViewController: UIViewController {
     var spaceView = UIView().then {
         $0.backgroundColor = .clear
     }
-
+    
     
     
     //MARK: - LifeCycle
@@ -148,11 +397,6 @@ class CelebrityProfileViewController: UIViewController {
         [scrollView].forEach{ view.addSubview($0) }
         [sayingEnglishLabel,sayingKoreaLabel].forEach{ wiseSayingView.addSubview($0)}
         
-        fourView.snp.makeConstraints{
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(400)
-        }
-        
         spaceView.snp.makeConstraints{
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(50)
@@ -162,7 +406,7 @@ class CelebrityProfileViewController: UIViewController {
         scrollView.snp.makeConstraints{
             $0.edges.equalToSuperview().inset(24)
         }
-
+        
         contentView.snp.makeConstraints{
             $0.width.equalTo(scrollView.snp.width).multipliedBy(1)
             $0.edges.equalTo(scrollView.contentLayoutGuide)
@@ -179,6 +423,42 @@ class CelebrityProfileViewController: UIViewController {
             $0.centerX.equalToSuperview()
             $0.bottom.equalToSuperview().inset(30)
         }
+        
+        
+        birthdayKeyLabel.snp.makeConstraints{
+            $0.width.equalTo(profileWidth)
+        }
+        
+        deceasedAtKeyLabel.snp.makeConstraints{
+            $0.width.equalTo(profileWidth)
+        }
+        
+        mbtiKeyLabel.snp.makeConstraints{
+            $0.width.equalTo(profileWidth)
+        }
+        nationalityKeyLabel.snp.makeConstraints{
+            $0.width.equalTo(profileWidth)
+        }
+        bodySpecKeyLabel.snp.makeConstraints{
+            $0.width.equalTo(profileWidth)
+        }
+        educationKeyLabel.snp.makeConstraints{
+            $0.width.equalTo(profileWidth)
+        }
+        jobKeyLabel.snp.makeConstraints{
+            $0.width.equalTo(profileWidth)
+        }
+        wealthKeyLabel.snp.makeConstraints{
+            $0.width.equalTo(profileWidth)
+        }
+        spouseKeyLabel.snp.makeConstraints{
+            $0.width.equalTo(profileWidth)
+        }
+        childrenKeyLabel.snp.makeConstraints{
+            $0.width.equalTo(profileWidth)
+        }
+        
+        
     }
     
     func setUI(){
