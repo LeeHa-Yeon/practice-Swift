dcdd4e3e9e3e3feb69b14482fb5ba6048d47b374
LeeHa-Yeon <gkdus1613@naver.com>
2022-04-28 01:38:08 +0900

    [FEAT] 기본 UI 구현

--- RxSwift/RxSiwft_GitHub/RxSiwft_GitHub/RepositoryListCell.swift
+++ RxSwift/RxSiwft_GitHub/RxSiwft_GitHub/RepositoryListCell.swift
@@ -5,4 +5,77 @@
 //  Created by 이하연 on 2022/04/27.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class RepositoryListCell: UITableViewCell {
+    
+    static let identifier = "repositoryListCell"
+    
+    var repository: Repository?
+    
+    let nameLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 15, weight: .bold)
+    }
+    let descriptionLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 15)
+        $0.numberOfLines = 2
+    }
+    let starImageView = UIImageView().then {
+        $0.image = UIImage(systemName: "star")
+    }
+    let starLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 16)
+        $0.textColor = .gray
+    }
+    let languageLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 16)
+        $0.textColor = .gray
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setLayout()
+        setData()
+    }
+    
+    func setLayout(){
+        [nameLabel,descriptionLabel,starImageView,starLabel,languageLabel].forEach{ contentView.addSubview($0)}
+        
+        nameLabel.snp.makeConstraints{
+            $0.top.leading.trailing.equalToSuperview().inset(18)
+        }
+        
+        descriptionLabel.snp.makeConstraints{
+            $0.top.equalTo(nameLabel.snp.bottom).offset(3)
+            $0.leading.trailing.equalTo(nameLabel)
+        }
+        
+        starImageView.snp.makeConstraints{
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(descriptionLabel)
+            $0.width.height.equalTo(20)
+            $0.bottom.equalToSuperview().inset(18)
+        }
+        
+        starLabel.snp.makeConstraints{
+            $0.centerY.equalTo(starImageView)
+            $0.leading.equalTo(starImageView.snp.trailing).offset(5)
+        }
+        
+        languageLabel.snp.makeConstraints{
+            $0.centerY.equalTo(starLabel)
+            $0.leading.equalTo(starLabel.snp.trailing).offset(12)
+        }
+    }
+    
+    func setData(){
+        guard let repository = repository else { return }
+        nameLabel.text = repository.name
+        descriptionLabel.text = repository.description
+        starLabel.text = "\(repository.stargazersCount)"
+        languageLabel.text = repository.language
+    }
+}
+
--- RxSwift/RxSiwft_GitHub/RxSiwft_GitHub/RepositoryListViewController.swift
+++ RxSwift/RxSiwft_GitHub/RxSiwft_GitHub/RepositoryListViewController.swift
@@ -5,4 +5,45 @@
 //  Created by 이하연 on 2022/04/27.
 //
 
-import Foundation
+import UIKit
+
+class RepositoryListViewController: UITableViewController {
+    private let organization = "Apple"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = organization + "Repositories"
+        self.refreshControl = UIRefreshControl()
+        let refreschControl = self.refreshControl!
+        refreschControl.backgroundColor = .white
+        refreschControl.tintColor = .darkGray
+        refreschControl.attributedTitle = NSAttributedString(string: "당겨서 새로고침")
+        refreschControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        
+        tableView.register(RepositoryListCell.self, forCellReuseIdentifier: RepositoryListCell.identifier)
+        tableView.rowHeight = 140
+    }
+    
+    func setLayout(){
+        
+    }
+    
+    func setTableView(){
+    }
+    
+    @objc func refresh(){
+        
+    }
+}
+
+extension RepositoryListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RepositoryListCell.identifier, for: indexPath) as? RepositoryListCell else { return UITableViewCell() }
+        return cell
+    }
+}
