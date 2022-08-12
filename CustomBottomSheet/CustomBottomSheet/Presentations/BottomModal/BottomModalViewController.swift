6bb5095d4ac13f4315996d9ee05f354b6e907b02
LeeHa-Yeon <gkdus1613@naver.com>
2022-08-12 18:31:37 +0900

    [CustomBottomSheet] 바텀모달 화면 구현

--- CustomBottomSheet/CustomBottomSheet/Presentations/BottomModal/BottomModalViewController.swift
+++ CustomBottomSheet/CustomBottomSheet/Presentations/BottomModal/BottomModalViewController.swift
@@ -11,8 +11,12 @@ import Then
 
 class BottomModalViewController: UIViewController {
     
+    private var sheetLeadingMargin: CGFloat = 24
+    private var sheetTralingMargin: CGFloat = 24
+    private var sheetTopMargin: CGFloat = 40
+    private var sheetBottomMargin: CGFloat = 34
+    
     // MARK: - Properties
-    var bottomHeight: CGFloat = 200 // 바텀 시트 높이
     private var bottomSheetViewTopConstraint: Constraint? = nil // bottomSheet와 View 상단에서 떨어진 거리
     
     // MARK: - Components
@@ -25,6 +29,13 @@ class BottomModalViewController: UIViewController {
         $0.layer.cornerRadius = 24
         $0.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         $0.clipsToBounds = true
+        $0.addSubview(containerStackView)
+    }
+    
+    private lazy var containerStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .equalSpacing
+        $0.spacing = 0
     }
     
 
@@ -49,33 +60,46 @@ class BottomModalViewController: UIViewController {
     
     // 레이아웃 세팅 작업
     private func setLayout(){
+        bottomSheetView.translatesAutoresizingMaskIntoConstraints = false
+        containerStackView.translatesAutoresizingMaskIntoConstraints = false
+        
         view.addSubview(blurredView)
         view.addSubview(bottomSheetView)
         
-        
-        let topConstant = view.safeAreaInsets.bottom + view.safeAreaLayoutGuide.layoutFrame.height
+        let safeAreaHeight: CGFloat = view.safeAreaLayoutGuide.layoutFrame.height
+        let bottomPadding: CGFloat = view.safeAreaInsets.bottom
         
         blurredView.snp.makeConstraints{
             $0.top.leading.trailing.bottom.equalToSuperview()
         }
         
-        bottomSheetView.snp.makeConstraints {
-            $0.leading.equalToSuperview()
-            $0.trailing.equalToSuperview()
+        bottomSheetView.snp.makeConstraints{
+            $0.leading.trailing.equalToSuperview()
             $0.bottom.equalToSuperview()
-            bottomSheetViewTopConstraint = $0.top.lessThanOrEqualTo(view.safeAreaLayoutGuide.snp.top).offset(topConstant).constraint
+            bottomSheetViewTopConstraint = $0.top.lessThanOrEqualTo(view.safeAreaLayoutGuide.snp.top).offset(safeAreaHeight + bottomPadding).constraint
+        }
+        
+        containerStackView.snp.makeConstraints{
+            $0.leading.trailing.equalToSuperview().inset(sheetLeadingMargin)
+            $0.top.equalToSuperview().inset(sheetTopMargin)
+            $0.bottom.equalToSuperview().inset(sheetBottomMargin)
         }
     }
+    
+    public func addComponents(targetStackView: UIStackView){
+        containerStackView = targetStackView
+    }
+    
     
     // 바텀 시트 표출 애니메이션
     private func showBottomSheet() {
         let safeAreaHeight: CGFloat = view.safeAreaLayoutGuide.layoutFrame.height
         let bottomPadding: CGFloat = view.safeAreaInsets.bottom
+        let bottomHeight: CGFloat = containerStackView.frame.height
         
         bottomSheetViewTopConstraint?.update(offset: (safeAreaHeight + bottomPadding) - bottomHeight )
         
         UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseIn, animations: {
-//            self.view.alpha = 0.5
             self.view.layoutIfNeeded()
         }, completion: nil)
     }
@@ -88,7 +112,6 @@ class BottomModalViewController: UIViewController {
         bottomSheetViewTopConstraint?.update(offset: safeAreaHeight + bottomPadding )
     
         UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseIn, animations: {
-//            self.view.alpha = 0.0
             self.view.layoutIfNeeded()
         }) { _ in
             if self.presentingViewController != nil {
@@ -103,11 +126,6 @@ class BottomModalViewController: UIViewController {
         let dimmedTap = UITapGestureRecognizer(target: self, action: #selector(dimmedViewTapped(_:)))
         blurredView.addGestureRecognizer(dimmedTap)
         blurredView.isUserInteractionEnabled = true
-        
-        // 스와이프 했을 때, 바텀시트를 내리는 swipeGesture
-//        let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(panGesture))
-//        swipeGesture.direction = .down
-//        view.addGestureRecognizer(swipeGesture)
     }
 
 
--- CustomBottomSheet/CustomBottomSheet/Presentations/ViewController.swift
+++ CustomBottomSheet/CustomBottomSheet/Presentations/ViewController.swift
@@ -9,9 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBAction func bottomSheetBtnTapped(_ sender: Any) {
-        let bottomSheetVC = BottomModalViewController()
-        bottomSheetVC.modalPresentationStyle = .overFullScreen
-        self.present(bottomSheetVC, animated: false)
+        showBottomModal()
     }
     
     override func viewDidLoad() {
@@ -22,3 +20,60 @@ class ViewController: UIViewController {
 
 }
 
+
+extension UIViewController {
+    
+    // 이용약관 동의
+    
+    // 애칭 설정 바텀시트 구조
+    
+    // 거주 국가 선택 구조
+    
+    func showBottomModal(){
+        let bottomSheetVC = BottomModalViewController()
+        
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "타이틀"
+        titleLabel.textAlignment = .center
+        titleLabel.textColor = .black
+        titleLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        titleLabel.numberOfLines = 0
+        
+        let contentLabel = UILabel()
+        contentLabel.text = "내용내뇽ㅇ내애내애낸앵"
+        contentLabel.textAlignment = .center
+        contentLabel.textColor = .black
+        contentLabel.font = .systemFont(ofSize: 16, weight: .bold)
+        contentLabel.numberOfLines = 0
+        
+        let titleLabel2 = UILabel()
+        titleLabel2.text = "타이틀"
+        titleLabel2.textAlignment = .center
+        titleLabel2.textColor = .black
+        titleLabel2.font = .systemFont(ofSize: 20, weight: .bold)
+        titleLabel2.numberOfLines = 0
+        
+        let contentLabel2 = UILabel()
+        contentLabel2.text = "내용내뇽ㅇ내애내애낸앵"
+        contentLabel2.textAlignment = .center
+        contentLabel2.textColor = .black
+        contentLabel2.font = .systemFont(ofSize: 16, weight: .bold)
+        contentLabel2.numberOfLines = 0
+        
+        let testStackView = UIStackView()
+        testStackView.axis = .vertical
+        testStackView.spacing = 0
+        testStackView.distribution = .equalSpacing
+        testStackView.addArrangedSubview(titleLabel)
+        testStackView.addArrangedSubview(contentLabel)
+        testStackView.addArrangedSubview(titleLabel2)
+        testStackView.addArrangedSubview(contentLabel2)
+        
+        
+        bottomSheetVC.addComponents(targetStackView: testStackView)
+        
+        bottomSheetVC.modalPresentationStyle = .overFullScreen
+        self.present(bottomSheetVC, animated: false)
+    }
+}
